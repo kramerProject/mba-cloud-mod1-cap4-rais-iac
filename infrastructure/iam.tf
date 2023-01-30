38a2e9808812504e47142c2d36c7580ef604fade
kramer <kramerscs@gmail.com>
2023-01-30 18:02:46 -0300

    Fix iam tf

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -34,7 +34,7 @@ resource "aws_iam_policy" "lambda" {
     policy = <<EOF
   {
     "Version": "2012-10-17",
-        "Statement": [
+    "Statement": [
       {
         "Effect": "Allow",
         "Action": [
