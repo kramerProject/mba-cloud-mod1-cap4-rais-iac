8fbd87a5be2d47a7e5b1d22e8d8f371725419917
kramer <kramerscs@gmail.com>
2023-02-02 16:03:01 -0300

    Adding dags

---
+++ airflow/dags/rais_processing.py
@@ -0,0 +1,120 @@
+from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
+import boto3
+from airflow.models import Variable
+
+aws_access_key_id = Variable.get("aws_access_key_id")
+aws_secret_access_key = Variable.get("aws_secret_access_key")
+
+client = boto3.client("emr", region_name="us-east-2",
+                    aws_access_key_id=aws_access_key_id,
+                    aws_secret_access_key=aws_secret_access_key)
+
+s3client = boto3.client("s3", aws_access_key_id=aws_access_key_id,
+                          aws_secret_access_key=aws_secret_access_key)
+
+
+# Usando a novíssima Taskflow API
+default_args = {
+    'owner': 'Neylson Crepalde',
+    "depends_on_past": False,
+    "start_date": days_ago(2),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False
+}
+
+@dag(default_args=default_args, schedule_interval=None, catchup=False, tags=["emr", "aws", "enem"], description="Pipeline for rais processing")
+def pipeline_rais():
+    """
+    Pipeline para processamento de dados do ENEM 2019.
+    """
+
+    @task
+    def emr_process_rais_data():
+        cluster_id = client.run_job_flow(
+            Name='EMR-Kramer-IGTI-delta-airflow',
+            ServiceRole='EMR_DefaultRole',
+            JobFlowRole='EMR_EC2_DefaultRole',
+            VisibleToAllUsers=True,
+            LogUri='s3://datalake-kramer-edc-tf-producao-401868797180/emr-logs',
+            ReleaseLabel='emr-6.3.0',
+            Instances={
+                'InstanceGroups': [
+                    {
+                        'Name': 'Master nodes',
+                        'Market': 'SPOT',
+                        'InstanceRole': 'MASTER',
+                        'InstanceType': 'm5.xlarge',
+                        'InstanceCount': 1,
+                    },
+                    {
+                        'Name': 'Worker nodes',
+                        'Market': 'SPOT',
+                        'InstanceRole': 'CORE',
+                        'InstanceType': 'm5.xlarge',
+                        'InstanceCount': 1,
+                    }
+                ],
+                'Ec2KeyName': 'kramer-igti-rais',
+                'KeepJobFlowAliveWhenNoSteps': True,
+                'TerminationProtected': False,
+                'Ec2SubnetId': 'subnet-005eeffbaafd46014'
+            },
+
+            Applications=[{'Name': 'Spark'}],
+
+            Configurations=[{
+                "Classification": "spark-env",
+                "Properties": {},
+                "Configurations": [{
+                    "Classification": "export",
+                    "Properties": {
+                        "PYSPARK_PYTHON": "/usr/bin/python3",
+                        "PYSPARK_DRIVER_PYTHON": "/usr/bin/python3"
+                    }
+                }]
+            },
+                {
+                    "Classification": "spark-hive-site",
+                    "Properties": {
+                        "hive.metastore.client.factory.class": "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"
+                    }
+                },
+                {
+                    "Classification": "spark-defaults",
+                    "Properties": {
+                        "spark.submit.deployMode": "cluster",
+                        "spark.speculation": "false",
+                        "spark.sql.adaptive.enabled": "true",
+                        "spark.serializer": "org.apache.spark.serializer.KryoSerializer"
+                    }
+                },
+                {
+                    "Classification": "spark",
+                    "Properties": {
+                        "maximizeResourceAllocation": "true"
+                    }
+                }
+            ],
+
+            Steps=[{
+                'Name': 'First Rais processing',
+                'ActionOnFailure': 'CONTINUE',
+                'HadoopJarStep': {
+                    'Jar': 'command-runner.jar',
+                    'Args': ['spark-submit',
+                            '--packages', 'io.delta:delta-core_2.12:1.0.0', 
+                            '--conf', 'spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension', 
+                            '--conf', 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog', 
+                            '--master', 'yarn',
+                            '--deploy-mode', 'cluster',
+                            's3://datalake-kramer-edc-tf-producao-401868797180/pyspark/spark_job.py'
+                        ]
+                }
+            }],
+        )
+        return cluster_id["JobFlowId"]
+
+
+execucao = pipeline_rais()
